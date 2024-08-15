ab37ae8765970d63d0d87509ad78e6128ed13926
quangtn <trannhatquang2001@gmail.com>
2024-08-15 14:08:52 +0700

    chore: update workflows

--- src/torino-restaurant-app/app/categories/create.tsx
+++ src/torino-restaurant-app/app/categories/create.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import CategoryForm from '@/components/categories/CategoryForm';
+import { API_URLS } from '@/constants/ApiUrls';
 
 export default function CategoryDetailScreen() {
+  const url = API_URLS.CATEGORIES.GET_LIST;
   return <CategoryForm isEdit={false} />;
 }
